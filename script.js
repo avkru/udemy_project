83382e9cd1473fef6a1d9dc52b2af2541f6a5e80
Ksandr <avk-freelancer@rambler.ru>
2022-08-17 16:17:03 +0300

    Создан объект appData из задачи 3

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 
 let money = prompt("Ваш бюджет на месяц?", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
+};
