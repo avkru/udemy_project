82d0bc1eea05103cc4c3be96fa83c4f07af548f5
Ksandr <avk-freelancer@rambler.ru>
2022-08-19 15:12:58 +0300

    Добавление свойства moneyPerDay объекта appData с определением достатка

--- script.js
+++ script.js
@@ -22,7 +22,18 @@ for (let i = 0; i < 2; i++) {
   } else {
 
   };
-
 };
 
-alert("Ваш бюджет на 1 день: " + (money / 30));
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ваш ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+  console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("Высокий уровень достатка");
+} else {
+  console.log("Произошла ошибка");
+}
