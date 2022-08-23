291a4ea0abefec16cf4043a424853541a72df00e
Ksandr <avk-freelancer@rambler.ru>
2022-08-23 15:15:59 +0300

    Добавлена функция detectLevel

--- script.js
+++ script.js
@@ -92,7 +92,11 @@ function detectDayBudget() {
   appData.moneyPerDay = +(appData.budget / 30).toFixed(2);
 
   alert("Ваш ежедневный бюджет: " + appData.moneyPerDay);
+}
 
+detectDayBudget();
+
+function detectLevel() {
   if (appData.moneyPerDay < 100) {
     console.log("Минимальный уровень достатка");
   } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
@@ -104,7 +108,7 @@ function detectDayBudget() {
   }
 }
 
-detectDayBudget();
+detectLevel();
 
 function checkSavings() {
   if (appData.savings == true) {
