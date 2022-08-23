4f9fe20b6094a93421477be793e1c67235553a65
Ksandr <avk-freelancer@rambler.ru>
2022-08-23 15:11:38 +0300

    Добавлена функция detectDayBudget

--- script.js
+++ script.js
@@ -88,19 +88,23 @@ function chooseExpenses() {
 
 chooseExpenses();
 
-appData.moneyPerDay = (appData.budget / 30).toFixed(2);
+function detectDayBudget() {
+  appData.moneyPerDay = +(appData.budget / 30).toFixed(2);
 
-alert("Ваш ежедневный бюджет: " + appData.moneyPerDay);
+  alert("Ваш ежедневный бюджет: " + appData.moneyPerDay);
 
-if (appData.moneyPerDay < 100) {
-  console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-  console.log("Средний уровень достатка");
-} else if (appData.moneyPerDay > 2000) {
-  console.log("Высокий уровень достатка");
-} else {
-  console.log("Произошла ошибка");
+  if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+  } else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+  } else {
+    console.log("Произошла ошибка");
+  }
 }
+
+detectDayBudget();
 
 function checkSavings() {
   if (appData.savings == true) {
